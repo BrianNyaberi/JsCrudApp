5ff5983e8b8d9cc7a20646eca862ee3a23d17f90
Web worker <briannyaberi1@gmail.com>
2022-05-31 18:40:11 +0300

    CRUD Api setup

---
+++ server/controller/controller.js
@@ -0,0 +1,21 @@
+var Userdb = require('../model/model')
+
+// create a new user
+exports.create = (req, res) => {
+    
+}
+
+// retrive and return a user(s)
+exports.find = (req, res) => {
+    
+}
+
+// update a user
+exports.update = (req, res) => {
+    
+}
+
+// delete a user
+exports.delete = (req, res) => {
+    
+}
