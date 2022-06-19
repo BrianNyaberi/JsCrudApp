5c9551690fb375321e10b4df290a7f9f500983b3
Web worker <briannyaberi1@gmail.com>
2022-06-19 15:23:26 +0300

    Make a request to api/users - axios.get

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,11 @@
+const axios = require('axios')
+
 exports.homeRoutes = (req, res) => {
-    res.render('index');
+    // Make a request to api/users
+    axios.get('http://localhost:3000/api/users')
+        .then(function (response) {
+            res.render('index', { users: response.data});
+        })
 }
 
 exports.add_user = (req, res) => {
