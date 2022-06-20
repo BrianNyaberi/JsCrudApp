8a5071e8bad4b5aa5fe59e760bf073b0baf656e0
Web worker <briannyaberi1@gmail.com>
2022-06-20 14:16:53 +0300

    Make a request to api/users, for a specific user

--- server/services/render.js
+++ server/services/render.js
@@ -4,7 +4,7 @@ exports.homeRoutes = (req, res) => {
     // Make a request to api/users
     axios.get('http://localhost:3000/api/users')
         .then(function (response) {
-            res.render('index', { users: response.data});
+            res.render('index', { users: response.data });
         })
 }
 
@@ -13,5 +13,12 @@ exports.add_user = (req, res) => {
 }
 
 exports.update_user = (req, res) => {
-    res.render('update_user');
+    // Make a request to api/users, for a specific user
+    axios.get('http://localhost:3000/api/users', { params: { id: req.query.id } })
+        .then(function (userdata) {
+            res.render('update_user', { user: userdata.data })
+        })
+        .catch(err => {
+            res.send(err)
+        })
 }
